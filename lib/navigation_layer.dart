f7dd20042c4e0a8fd7b697a3d9ec7ac8f475e007
Victor Manuel Quintana Díaz <vmquintanasnow@gmail.com>
2022-08-15 12:31:32 +0200

    Update: navigation layer documentation

--- lib/navigation_layer.dart
+++ lib/navigation_layer.dart
@@ -1,11 +1,12 @@
 part of 'app_navigator.dart';
 
+///A widget that manages all the navigation changes.
+///Uses a imperative declaration follow the Navigator 2.0 guidelines.
 class NavigationLayer extends StatelessWidget {
   final Widget initPage;
   final String initPath;
 
-  NavigationLayer({Key? key, required this.initPage, required this.initPath})
-      : super(key: key) {
+  NavigationLayer({Key? key, required this.initPage, required this.initPath}) : super(key: key) {
     AppNavigator().pages.value = [AppPage(child: initPage, name: initPath)];
   }
 
